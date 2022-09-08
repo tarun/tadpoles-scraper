8fe52cfab175634bb130086940d31c40718a5400
Tarun Kothuri <tarun.codex@gmail.com>
2022-09-08 00:40:01 -0400

    add support for using `cookie_header` for authentication
    add support for using `cookie_header` for authentication (e.g. with google sign-in)

--- index.js
+++ index.js
@@ -33,7 +33,11 @@ const config = require('./config.json')
  */
 axiosCookieJarSupport(axios)
 const cookieJar = new tough.CookieJar()
-
+if (config.cookie_header) {
+	cookieJar.setCookie(
+		tough.Cookie.parse(config.cookie_header)
+		, "https://www.tadpoles.com", {}, console.log)
+}
 const client = axios.create({
 	baseURL: 'https://www.tadpoles.com',
 	jar: cookieJar,
@@ -58,7 +62,7 @@ async function authenticate() {
 	} catch (error) {
 		console.log('Authentication Error')
 		process.exit(1)
-	}	
+	}
 }
 
 /**
@@ -91,7 +95,7 @@ async function admit() {
 		console.log('Admission Error')
 		console.log(error)
 		process.exit(1)
-	}	
+	}
 }
 
 /**
@@ -106,6 +110,7 @@ async function get_overview() {
 	parms = {
 		'include_all_kids': 'true',
 		'include_guardians': 'false'
+		// 'obj': authObj
 	}
 
 	try {
@@ -114,7 +119,7 @@ async function get_overview() {
 		console.log('Error retreiving list')
 		console.log(error)
 		process.exit(1)
-	}	
+	}
 }
 
 /**
@@ -140,7 +145,7 @@ async function get_events(lastdate) {
 		console.log('Error retreiving events')
 		console.log(error)
 		process.exit(1)
-	}	
+	}
 }
 
 /**
@@ -157,7 +162,7 @@ async function maybe_download_attachment(imgkey, event) {
 	date = event.event_date.split('-')
 	imghash = crypto.createHash('md5').update(imgkey).digest('hex')
 	imgurl = '/remote/v1/attachment?key=' + imgkey
-  
+
 	var dirbase = config.image_path
 	dirbase = dirbase.split('%child%').join(event.parent_member_display)
 	dirbase = dirbase.split('%YYYY%').join(date[0])
@@ -173,10 +178,10 @@ async function maybe_download_attachment(imgkey, event) {
 	filebase = filebase.split('%imgkey%').join(imgkey)
 
 	filename = false
-	
-	if (! fs.existsSync(dirbase + filebase + '.jpg' ) && ! fs.existsSync(dirbase + filebase + '.png' ) && ! fs.existsSync(dirbase + filebase + '.mp4' ) && ! fs.existsSync(dirbase + filebase + '.pdf' )) {
+
+	if (!fs.existsSync(dirbase + filebase + '.jpg') && !fs.existsSync(dirbase + filebase + '.png') && !fs.existsSync(dirbase + filebase + '.mp4') && !fs.existsSync(dirbase + filebase + '.pdf')) {
 		console.log('    -- File ' + filebase + ' from ' + event.event_date + ' does not exist... downloading')
-		await mkdirp( dirbase, function(err) {
+		await mkdirp(dirbase, function (err) {
 			if (err != null) {
 				console.error('error calling mkdirp on path: ', dirbase, '\n\ndetails: ', err)
 			}
@@ -192,23 +197,22 @@ async function maybe_download_attachment(imgkey, event) {
 			await client.get(imgurl, {
 				responseType: 'arraybuffer',
 				headers: {
-				  'Accept': '*/*'
+					'Accept': '*/*'
 				}
 			}).then(function (response) {
-				if (response.headers['content-type'] == 'image/jpeg') {
-					filename = dirbase + filebase + '.jpg'
-					fs.writeFileSync(filename, response.data)
-				} else if (response.headers['content-type'] == 'image/png') {
-					filename = dirbase + filebase + '.png'
-					fs.writeFileSync(filename, response.data)
-				} else if (response.headers['content-type'] == 'video/mp4') {
-					filename = dirbase + filebase + '.mp4'
-					fs.writeFileSync(filename, response.data)
-				} else if (response.headers['content-type'] == 'application/pdf') {
-					filename = dirbase + filebase + '.pdf'
+				const contentType = response.headers['content-type'];
+				let extension = {
+					'image/jpeg': 'jpg',
+					'image/png': 'png',
+					'video/mp4': 'mp4',
+					'application/pdf': 'pdf',
+					// 'application/x-mpegURL': '.m3u8'
+				}[contentType];
+				if (extension) {
+					filename = dirbase + filebase + '.' + extension
 					fs.writeFileSync(filename, response.data)
 				} else {
-					console.log('      -- content-type ' + response.headers['content-type'] + ' excluded - skipping' )
+					console.log('      -- content-type ' + contentType + ' excluded - skipping')
 				}
 			})
 			return filename
@@ -232,7 +236,7 @@ async function maybe_change_fileext(file) {
 
 	var fileext = file.split('.').pop()
 
-	if ( fileext == 'jpg' || fileext == 'png' ) {
+	if (fileext == 'jpg' || fileext == 'png') {
 		var exts = imghdr.what(file)
 		if (exts.indexOf(fileext) === -1) {
 			basename = file.replace(/\.[^/.]+$/, "")
@@ -262,8 +266,8 @@ async function maybe_update_image_data(file, event) {
 	var fileext = file.split('.').pop()
 	var event_date = new Date(event.event_time * 1000)
 
-	if ( fileext == 'jpg' ) {
-		formatted_date = event_date.getFullYear() + ':' + ("0" + (event_date.getMonth() + 1)).slice(-2) +  ':' + ("0" + (event_date.getDate())).slice(-2) + ' ' + ("0" + (event_date.getHours())).slice(-2) + ':'  + ("0" + (event_date.getMinutes())).slice(-2) + ':' + ("0" + (event_date.getSeconds())).slice(-2)
+	if (fileext == 'jpg') {
+		formatted_date = event_date.getFullYear() + ':' + ("0" + (event_date.getMonth() + 1)).slice(-2) + ':' + ("0" + (event_date.getDate())).slice(-2) + ' ' + ("0" + (event_date.getHours())).slice(-2) + ':' + ("0" + (event_date.getMinutes())).slice(-2) + ':' + ("0" + (event_date.getSeconds())).slice(-2)
 
 		var img = fs.readFileSync(file)
 		var data = img.toString('binary')
@@ -279,8 +283,8 @@ async function maybe_update_image_data(file, event) {
 		})
 	}
 
-	if ( fileext == 'png' ) {
-		formatted_date = event_date.getFullYear() + ':' + ("0" + (event_date.getMonth() + 1)).slice(-2) +  ':' + ("0" + (event_date.getDate())).slice(-2) + ' ' + ("0" + (event_date.getHours())).slice(-2) + ':'  + ("0" + (event_date.getMinutes())).slice(-2) + ':' + ("0" + (event_date.getSeconds())).slice(-2)
+	if (fileext == 'png') {
+		formatted_date = event_date.getFullYear() + ':' + ("0" + (event_date.getMonth() + 1)).slice(-2) + ':' + ("0" + (event_date.getDate())).slice(-2) + ' ' + ("0" + (event_date.getHours())).slice(-2) + ':' + ("0" + (event_date.getMinutes())).slice(-2) + ':' + ("0" + (event_date.getSeconds())).slice(-2)
 
 		var img = fs.readFileSync(file)
 		var chunks = extract(img)
@@ -293,37 +297,47 @@ async function maybe_update_image_data(file, event) {
 		})
 	}
 
-	await utimes.utimes(file, event_date.getTime(), event_date.getTime(), event_date.getTime(), function(){})
+	await utimes.utimes(file, event_date.getTime(), event_date.getTime(), event_date.getTime(), function () { })
 
 }
 
-async function main () {
+async function main() {
 	console.log('Starting Tadpoles Scraper...')
-	auth = await authenticate()
-	admit = await admit()
+	if (config.login_type === 'email') {
+		auth = await authenticate()
+		admit = await admit()
+	}
+	
 	info = await get_overview()
 	finalitem = info.data.first_event_time
 	lastitem = info.data.last_event_time + 1000
 
-	while ( finalitem != lastitem ) {
+	let i = 1
+
+	while ((finalitem != lastitem) && i--) {
 
 		// We already iteratred over this so we are probably stuck in a loop
-		if(iterations.indexOf(lastitem) != -1) {
+		if (iterations.indexOf(lastitem) != -1) {
 			console.log('Ending loop')
 			break
 		}
 
 		list = await get_events(lastitem)
 		for (const event of list.data.events) {
-			for (const imgkey of event.attachments){
-				if( event.event_time < lastitem ) {
+			for (const imgkey of event.attachments) {
+				if (event.event_time < lastitem) {
 					lastitem = event.event_time
 				}
-				
-				download = await maybe_download_attachment( imgkey, event )
-				if(download){
-					filename = await maybe_change_fileext(download) 
-					await maybe_update_image_data(filename, event)
+
+				try {
+					download = await maybe_download_attachment(imgkey, event)
+					if (download) {
+						filename = await maybe_change_fileext(download)
+						await maybe_update_image_data(filename, event)
+					}
+				} catch (e) {
+					// ignore error gracefully proceed to next item, if there is an error downloading or processing
+					console.error(e)
 				}
 			}
 		}
